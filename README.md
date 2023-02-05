21aae092180a75ef65419655c4fe388ca233b352
Monica Zarate <monicazaratecortes@gmail.com>
2023-02-04 22:54:43 -0800

    Updated Readme file.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# BCIT 3294 - Notes Application
-- Add documentation below 
+# BCIT 4590 - Notes Application
+
+## Quality Assurance Project
+
+Hello and welcome to this Note-taking application!
+
+- This site makes use of CRUD operations to access a SQL database of courses notes. It has the ability to create new entries, as wells as read, edit and delete existing ones.
+
+- The structure of this app consists on two main sections: The app folder, were the Classes, Functions and Init files are located.
+
+- The second section is the public folder, which contains all the mark-up for the website, and is reaching to the database through the logic stated on the app folder and prints out the notes on the browser by making use of partials files, like the header and footer sections, the card component, as well as the create, edit and delete note pages.
+
+- For this second iteration of this project, I've also added the users folder, inside of the public folder, where we find the logic to create a new user, login and logout of the application.
--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-    // QA - Assignment 1
-
-    // Hello and welcome to this Note-taking application!
-    // This site makes use of CRUD operations to access a SQL database of courses notes. It has the ability to create new entries, as wells as read, edit and delete existing ones.
-
-    // The structure of this app consists on two main sections: The app folder, were the Classes, Functions and Init files are located. 
-    // The second section is the public folder, which contains all the mark-up for the website, and is reaching to the database through the logic stated on the app folder and prints out the notes on the browser by making use of partials files, like the header and footer sections, the card component, as well as the create, edit and delete note pages.
-    // For this second iteration of this project, I've also added the users folder, inside of the public folder, where we find the logic to create a new user, login and logout of the application.
-
     // To start running our app, I am importing the 'init' file from the app folder, that file is important to be accessible throughout the application, since it contains the site's constants, has access to the functions and classes files, as well as the database connection. 
 
     // I am calling the find_all() method from the Note Class, this time making use of the user id we retrieve from the session's Class is_logged_in() method, which returns all the notes from the database for each different user accordingly, and we are assigning that data to the $notes variable. We are making use of the loop partial, which is were we map out each of the notes and passing them through the fetch_assoc() method, to retrieve each note in a associative array form, to access their key and values information. This information gets rendered on the page with the help of the Card partial.
